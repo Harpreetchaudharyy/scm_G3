ac59dae25531dda802e46a9d538a9c1a133db273
Harpreetchaudharyy <harpreetchaudhary100@gmail.com>
2025-01-30 14:37:40 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main(){
+  int day;
+    printf("Enter the number from a week (1-7): ");
+    scanf("%d", &day);
+
+    if (day == 1){
+        printf("Monday");
+    }
+    else if (day == 2){
+        printf("Tueday");
+    }
+    else if (day == 3){
+        printf("Wednesday");
+    }
+    else if (day == 4){
+        printf("Thursday");
+    }
+    else if (day == 5){
+        printf("Friday");
+    }
+    else if (day == 6){
+        printf("Saturday");
+    }
+    else if (day == 7){
+        printf("Sunday");
+    }
+    else{
+        printf("invalid output");
+    }
+  return 0;
+}
